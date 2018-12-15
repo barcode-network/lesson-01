54ffdbe6fae24c8a7ac5ae107fd9d07b2804f893
Shannon Clarke <>
2018-12-15 14:05:16 -0400

    Moved CSS and JS to external files

---
+++ script.js
@@ -0,0 +1,13 @@
+function randomFunc(){
+
+    const sampleText = document.getElementById('sampleText').innerHTML
+    /* sampleText = 'Dog President' */
+    alert(sampleText)
+    
+  }
+
+
+  function findText(value){
+  alert(value === 'b')
+  return value == 'b' //true if equivalent and false if not
+  }
